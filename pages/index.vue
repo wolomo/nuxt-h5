e30ea616a767c7b155f1688b70191b2626d710b8
wolomo <1922392531@qq.com>
2020-09-13 13:44:02 +0800

    生命周期

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,52 @@
 </template>
 
 <script>
-export default {}
+import middleware from "../.nuxt/middleware";
+
+export default {
+    //SSR   无法得到windows对象
+    // 中间件
+    middleware(context){
+        // console.log(context)
+        console.log("middleware page")
+    },
+    // 参数有效性
+    validate({params,query}){
+      console.log("validate")
+        return true
+    },
+    // 读数据，返回组件
+    asyncData(context){
+        console.log("asyncData")
+    },
+    // 读数据，返回vuex
+    fetch(){
+        return{
+            b:1
+        }
+    },
+    //SSR &CSR
+    beforeCreate(){
+        console.log("beforeCreate")
+    },
+    created(){
+        console.log("created")
+    },
+    //CSR  无法得到服务端对象context
+    // 可以得到windows，this
+    data(){
+        return{
+            a:1
+        }
+    },
+    mounted() {
+        console.log("mounted")
+    },
+    updated() {
+        console.log("updated")
+    }
+
+}
 </script>
 
 <style>
